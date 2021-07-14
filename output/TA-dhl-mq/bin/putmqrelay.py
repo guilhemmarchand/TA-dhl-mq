87ddfbe78a3ced73be0b60d5a309dd51033d6bc8
Guilhem Marchand <guilhem.marchand@gmail.com>
2021-07-14 09:22:53 +0100

    current beta too slow

--- output/TA-dhl-mq/bin/putmqrelay.py
+++ output/TA-dhl-mq/bin/putmqrelay.py
@@ -12,6 +12,7 @@ import csv
 import uuid
 import requests
 import json
+import hashlib
 
 splunkhome = os.environ['SPLUNK_HOME']
 sys.path.append(os.path.join(splunkhome, 'etc', 'apps', 'TA-dhl-mq', 'lib'))
@@ -59,6 +60,14 @@ class PutMqRelay(StreamingCommand):
         **Description:** field name containing the message to be published.''',
         require=True)
 
+    dedup = Option(
+        doc='''
+        **Syntax:** **dedup=****
+        **Description:** verify that the message MD5 sum exists already in the KVstore or not.
+        If true, refuse to add this record to the KVstore.
+        If false use a random record for the key generation.
+        .''',
+        require=False, validate=validators.Match("dedup", r"^(True|False)$"))
 
     # This function is required to handle special chars for storing the object in the KVstore
     def checkstrforjson(i):
@@ -109,6 +118,10 @@ class PutMqRelay(StreamingCommand):
                     if key == "no_max_retry":
                         no_max_retry = value
 
+        # Set the dedup mode
+        if not self.dedup:
+            self.dedup = 'False'
+
         # Loop in the results
         for record in records:
 
@@ -122,8 +135,11 @@ class PutMqRelay(StreamingCommand):
             # status is pending for an addition
             status = 'pending'
 
-            # generate a random uuid to name the record
-            keyrecord = uuid.uuid4()
+            # use the MD5 sum of the message as a unique key identifier
+            if self.dedup == 'True':
+                keyrecord = hashlib.md5(message.encode('utf-8')).hexdigest()
+            else:
+                keyrecord = uuid.uuid4()
 
             # Get some requires fields length for reporting and verification purposes
             appname_len = len(appname)
@@ -144,32 +160,53 @@ class PutMqRelay(StreamingCommand):
             # Insert in the KV
             if message and (message_len>0 and appname_len>0 and region_len>0 and manager_len>0 and channel_len>0 and queue_len>0):
 
-                try:
+                # if dedup, verifies if a key record exists already, otherwise isdup is False
+                isdup = False
+                
+                if self.dedup == 'True':
 
-                    # Insert the record
-                    collection.data.insert(json.dumps({
-                        "_key": str(keyrecord),
-                        "ctime": str(ctime),
-                        "mtime": str(mtime),
-                        "status": "pending",
-                        "manager": str(manager),
-                        "channel": str(channel),
-                        "queue": str(queue),
-                        "appname": str(appname),
-                        "region": str(region),
-                        "no_attempts": "0",
-                        "no_max_retry": str(no_max_retry),
-                        "user": str(user),
-                        "message": str(message)
-                        }))
+                    record = None
+                    try:
+                        # Get the record
+                        record = json.dumps(collection.data.query_by_id(keyrecord), indent=1)
+                        isdup = True
+                    except Exception as e:
+                        isdup = False
 
-                    action = "success"
-                    result = "record key: " + str(keyrecord) + " successfully added to the MQ publishing KVstore"
+                # if not a dup, or dedup is not enabled
+                if not isdup:
 
-                except Exception as e:
+                    try:
 
+                        # Insert the record
+                        collection.data.insert(json.dumps({
+                            "_key": str(keyrecord),
+                            "ctime": str(ctime),
+                            "mtime": str(mtime),
+                            "status": "pending",
+                            "manager": str(manager),
+                            "channel": str(channel),
+                            "queue": str(queue),
+                            "appname": str(appname),
+                            "region": str(region),
+                            "no_attempts": "0",
+                            "no_max_retry": str(no_max_retry),
+                            "user": str(user),
+                            "message": str(message)
+                            }))
+
+                        action = "success"
+                        result = "record key: " + str(keyrecord) + " successfully added to the MQ publishing KVstore"
+
+                    except Exception as e:
+
+                        action = "failure"
+                        result = "KVstore saving has failed!. Exception: " + str(e)
+
+                else:
                     action = "failure"
-                    result = "KVstore saving has failed!. Exception: " + str(e)
+                    result = "dedup detected, a record with the same MD5 hash exists already in the KVstore"
+                    status = "refused"
 
             else:
                 action = "failure"
